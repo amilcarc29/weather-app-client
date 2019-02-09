c1e0332509997245515a4149befac038c3c9d91b
Amilcar Cattaneo <amilcar.cattaneo29@gmail.com>
2019-02-09 18:30:17 -0300

    Adds API Documentation.

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const apiKey = "2e53218b7e31aa63d6596df481834fab";
 
+// https://openweathermap.org/current#data
+
 export default {
   getWeather: city =>
     axios.get(
